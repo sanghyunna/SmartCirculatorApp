f7ff5bad616e72f4528e2e82b6140bfc73ce8bdd
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-09-30 22:04:05 +0900

    Update message_sender.dart

--- practice/lib/message_sender.dart
+++ practice/lib/message_sender.dart
@@ -2,21 +2,16 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class MessageSender {
-  final String apiUrl = 'http://127.0.0.1:8000/connection/control';
+  final String apiUrl = 'http://circulator.p-e.kr:8000/connection/control/';
 
-  // Function to send the POST request.
   Future<void> sendMessage(String message) async {
     try {
-      // Create a map containing the data you want to send as JSON.
       Map<String, dynamic> messageData = {
         'message': message,
-        // Add more key-value pairs as needed.
       };
 
-      // Convert the map to a JSON string.
       String jsonBody = json.encode(messageData);
 
-      // Send the POST request.
       final response = await http.post(
         Uri.parse(apiUrl),
         headers: {
@@ -25,16 +20,12 @@ class MessageSender {
         body: jsonBody,
       );
 
-      // Check the response status code.
       if (response.statusCode == 200) {
-        // Request was successful.
         print('Message sent successfully');
       } else {
-        // Request failed.
         print('Failed to send message. Status code: ${response.statusCode}');
       }
     } catch (e) {
-      // Handle any exceptions that occur during the request.
       print('Error sending message: $e');
     }
   }
