acc7858158cc52506776ab197a7439f1b3b9a9aa
sanghyunna <106161726+sanghyunna@users.noreply.github.com>
2023-09-30 22:04:23 +0900

    server : added controls

---
+++ server/connection/service.py
@@ -0,0 +1,15 @@
+
+
+def control_up():
+    pass
+def control_down():
+    pass
+def control_left():
+    pass
+def control_right():
+    pass
+
+def control_speed_up():
+    pass
+def control_speed_down():
+    pass
--- server/connection/views.py
+++ server/connection/views.py
@@ -1,13 +1,33 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
+from service import *
 
 @csrf_exempt
 def control(request):
     if request.method == 'POST':
         print(request.body)
-        return JsonResponse({
-            'status': 'ok'
-            })
+        res = request.body.message
+        if res == "Up":
+            control_up()
+        elif res == "Down":
+            control_down()
+        elif res == "Left":
+            control_left()
+        elif res == "Right":
+            control_right()
+        elif res == "Speed Up":
+            control_speed_up()
+        elif res == "Speed Down":
+            control_speed_down()
+        else:
+            return JsonResponse({
+                'status': 'error',
+                'message': 'unknown message'
+                }, status = 404)
+            
     else:
-        return JsonResponse({'status': 'error'})
+        return JsonResponse({
+            'status': 'error',
+            'message': 'only POST requests are allowed'
+            }, status = 403)
